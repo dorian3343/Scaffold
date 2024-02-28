4caf9d71aef77f844693d53030b64fd31a506b41
dorian3343 <dorian3343@gmail.com>
2024-02-28 17:13:38 +0100

    Add build time message

--- Scaffold.go
+++ Scaffold.go
@@ -8,9 +8,12 @@ import (
 	"service/configuration"
 	"service/controller"
 	"service/misc"
+	"time"
 )
 
 func main() {
+	// measure time timetostart
+	start := time.Now()
 	// Set pretty logging straight away
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	misc.WelcomeMessage()
@@ -21,6 +24,8 @@ func main() {
 
 	//Attach Controllers to HTTP + Start the HTTP server
 	controller.SetupControllers(conf.Server.Services)
-
+	end := time.Now()
+	elapsed := end.Sub(start)
+	log.Info().Msgf("Project built in : %s", elapsed)
 	misc.StartHttp(conf.Server.Port)
 }
