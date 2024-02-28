9a5aba196ee09538181930009f44456760d01f23
dorian3343 <dorian3343@gmail.com>
2024-02-28 01:53:52 +0100

    Better Doc's + moved Capitalize to here

--- misc/Misc.go
+++ misc/Misc.go
@@ -7,11 +7,13 @@ import (
 	"strconv"
 )
 
+// A simple message to display on startup
 func WelcomeMessage() {
 	text := "Thank you for using Scaffold! Happy dev'ing <3 !"
 	fmt.Println(text)
 }
 
+// Startup a http server on a port
 func StartHttp(port int) {
 	log.Info().Msg("Starting HTTP server...")
 	err := http.ListenAndServe(":"+strconv.Itoa(port), nil)
@@ -19,4 +21,12 @@ func StartHttp(port int) {
 		log.Fatal().Err(err).Msg("Fatal Error with http server")
 	}
 
+}
+
+// Capitalize a string
+func Capitalize(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+	return string(s[0]-'a'+'A') + s[1:]
 }
